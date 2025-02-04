49f255d9bb01cd5df99520613b7b1ef99abad7b3
Kruglov Sergei <skruglov17@gmail.com>
2025-02-04 22:30:02 +0300

    Подключение архетипа JavaFX к Maven
    Подключение архетипа JavaFX к Maven

---
+++ Maven/SystemInfo.java
@@ -0,0 +1,13 @@
+package org.main;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ Maven/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module org.main {
+    requires javafx.controls;
+    exports org.main;
+}
