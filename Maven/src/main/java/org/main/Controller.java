cc64707e5a9282ae2487e1cd1c166c0abe5b0d87
Kruglov Sergey <Skruglov17@gmail.com>
2025-03-20 18:41:42 +0300

    Поправлена форма и начат процесс добавления подзадачи

--- Maven/src/main/java/org/main/Controller.java
+++ Maven/src/main/java/org/main/Controller.java
@@ -30,7 +30,13 @@ public class Controller implements Initializable {
     @FXML
     private ToggleButton priorityOne, priorityTwo, priorityThree, priorityFour;
     @FXML
-    private TextField topic, topicSubtask, responsible, dateRegistration, timeRegistration, dateWish, timeWish, dateComplete, timeComplete;
+    private TextField topic, topicSubtask, responsible;
+    @FXML
+    private TextField dayRegistration, monthRegistration, yearRegistration, hourRegistration, minuteRegistration, secondRegistration;
+    @FXML
+    private TextField dayWish, monthWish, yearWish, hourWish, minuteWish, secondWish;
+    @FXML
+    private TextField dayComplete, monthComplete, yearComplete, hourComplete, minuteComplete, secondComplete;
     @FXML
     private CheckBox checkComplete;
     @FXML
@@ -51,113 +57,149 @@ public class Controller implements Initializable {
 
     @FXML
     private void addSubtask() {
-        String nameTopicSubtask = topicSubtask.getText();
-        Connection connection = DBConnector.getConnection();
-        LinkedList<Task> tasks = new LinkedList<>();
-        try (
-                Statement statement = connection.createStatement();
-                ResultSet resultSet = statement.executeQuery("SELECT * FROM tasks")
-        ) {
-            while(resultSet.next()) {
-                tasks.add(mapRowTask(resultSet));
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+//        int idParentTask;
+//        SelectionModel<TreeItem<Task>> selectionModel = tasksTree.getSelectionModel();
+//        selectionModel.selectedItemProperty().addListener(new ChangeListener<TreeItem<Task>>() {
+//            public void changed(ObservableValue<? extends TreeItem<Task>> changed,
+//                TreeItem<Task> unSelectedValue, TreeItem<Task> selectedValue) {
+//                    Connection connection = DBConnector.getConnection();
+//                    LinkedList<Task> tasks = new LinkedList<>();
+//                    Task subTask = new Task();
+//                    try (
+//                            Statement statement = connection.createStatement();
+//                            ResultSet resultSet = statement.executeQuery("INSERT INTO tasks (topic, create_date, parental_task) VALUES ('" + topicSubtask.getText() + "', '" + OffsetDateTime.now() + "', '" + +"') RETURNING id");
+//                    ) {
+//                        while (resultSet.next()) {
+//                            subTask.setId();
+//                        }
+//                    } catch (SQLException e) {
+//                        throw new RuntimeException(e);
+//                    }
+//            }
+//        }
     }
 
     /**
-     * Метод предназначен для заполнения данных по выбранной задаче из списка
+     * Метод предназначен для получения выбранной задачи из списка
      */
     private void addListener() {
+        TreeItem<Task> selectedTask;
         SelectionModel<TreeItem<Task>> selectionModel = tasksTree.getSelectionModel();
         selectionModel.selectedItemProperty().addListener(new ChangeListener<TreeItem<Task>>(){
             public void changed(ObservableValue<? extends TreeItem<Task>> changed,
                                 TreeItem<Task> unSelectedValue, TreeItem<Task> selectedValue){
-                //Заполнение темы задачи
-                topic.setText(selectedValue.getValue().getTopic());
-                //Заполнение приоритета задачи
-                PriorityEnum priorityTask = selectedValue.getValue().getPriority();
-                if(priorityTask != null) {
-                    switch(priorityTask) {
-                        case ONE:
-                            priorityOne.setSelected(true);
-                            priorityTwo.setSelected(false);
-                            priorityThree.setSelected(false);
-                            priorityFour.setSelected(false);
-                            break;
-                        case TWO:
-                            priorityOne.setSelected(false);
-                            priorityTwo.setSelected(true);
-                            priorityThree.setSelected(false);
-                            priorityFour.setSelected(false);
-                            break;
-                        case THREE:
-                            priorityOne.setSelected(false);
-                            priorityTwo.setSelected(false);
-                            priorityThree.setSelected(true);
-                            priorityFour.setSelected(false);
-                            break;
-                        case FOUR:
-                            priorityOne.setSelected(false);
-                            priorityTwo.setSelected(false);
-                            priorityThree.setSelected(false);
-                            priorityFour.setSelected(true);
-                            break;
-                        default:
-                            System.out.println("Ошибка определения приоритета по задаче!");
-                    }
-                } else {
-                    priorityOne.setSelected(false);
+                                    updateTreeTasks(selectedValue);
+            }
+        });
+    }
+
+    private void updateTreeTasks(TreeItem<Task> selectedTask) {
+        //Заполнение темы задачи
+        topic.setText(selectedTask.getValue().getTopic());
+        //Заполнение приоритета задачи
+        PriorityEnum priorityTask = selectedTask.getValue().getPriority();
+        if(priorityTask != null) {
+            switch(priorityTask) {
+                case ONE:
+                    priorityOne.setSelected(true);
                     priorityTwo.setSelected(false);
                     priorityThree.setSelected(false);
                     priorityFour.setSelected(false);
-                }
-                //Заполнение ответственного по задаче
-                responsible.setText(selectedValue.getValue().getResponsiblePerson());
-                //Заполнение даты и времени регистрации
-                OffsetDateTime registrationTask = selectedValue.getValue().getCreateDate();
-                if(registrationTask != null) {
-                    dateRegistration.setText(registrationTask.toLocalDate().toString());
-                    timeRegistration.setText(registrationTask.toLocalTime().toString());
-                } else {
-                    dateRegistration.setText("");
-                    timeRegistration.setText("");
-                }
-                //Заполнение даты и времени желаемой даты реализации
-                OffsetDateTime wishDateTask = selectedValue.getValue().getWishDate();
-                if(wishDateTask != null) {
-                    dateWish.setText(wishDateTask.toLocalDate().toString());
-                    timeWish.setText(wishDateTask.toLocalTime().toString());
-                } else {
-                    dateWish.setText("");
-                    timeWish.setText("");
-                }
-                //Заполнение даты и времени даты выполнения задачи
-                OffsetDateTime completeDateTask = selectedValue.getValue().getCompleteDate();
-                if(completeDateTask != null) {
-                    dateComplete.setText(completeDateTask.toLocalDate().toString());
-                    timeComplete.setText(completeDateTask.toLocalTime().toString());
-                } else {
-                    dateComplete.setText("");
-                    timeComplete.setText("");
-                }
-                //Проставление статуса выполнения задачи
-                StatusEnum completeCheckStatus = selectedValue.getValue().getStatus();
-                if(completeCheckStatus == StatusEnum.COMPLETE) {
-                    checkComplete.setSelected(true);
-                } else {
-                    checkComplete.setSelected(false);
-                }
-                //Заполнение описания задачи
-                String descriptionTask = selectedValue.getValue().getDescription();
-                if(descriptionTask != null) {
-                    description.setText(descriptionTask);
-                } else {
-                    description.setText("");
-                }
+                    break;
+                case TWO:
+                    priorityOne.setSelected(false);
+                    priorityTwo.setSelected(true);
+                    priorityThree.setSelected(false);
+                    priorityFour.setSelected(false);
+                    break;
+                case THREE:
+                    priorityOne.setSelected(false);
+                    priorityTwo.setSelected(false);
+                    priorityThree.setSelected(true);
+                    priorityFour.setSelected(false);
+                    break;
+                case FOUR:
+                    priorityOne.setSelected(false);
+                    priorityTwo.setSelected(false);
+                    priorityThree.setSelected(false);
+                    priorityFour.setSelected(true);
+                    break;
+                default:
+                    System.out.println("Ошибка определения приоритета по задаче!");
             }
-        });
+        } else {
+            priorityOne.setSelected(false);
+            priorityTwo.setSelected(false);
+            priorityThree.setSelected(false);
+            priorityFour.setSelected(false);
+        }
+        //Заполнение ответственного по задаче
+        responsible.setText(selectedTask.getValue().getResponsiblePerson());
+        //Заполнение даты и времени регистрации
+        OffsetDateTime registrationTask = selectedTask.getValue().getCreateDate();
+        if(registrationTask != null) {
+            dayRegistration.setText(Integer.toString(registrationTask.getDayOfMonth()));
+            monthRegistration.setText(Integer.toString(registrationTask.getMonthValue()));
+            yearRegistration.setText(Integer.toString(registrationTask.getYear()));
+            hourRegistration.setText(Integer.toString(registrationTask.getHour()));
+            minuteRegistration.setText(Integer.toString(registrationTask.getMinute()));
+            secondRegistration.setText(Integer.toString(registrationTask.getSecond()));
+        } else {
+            dayRegistration.setText("");
+            monthRegistration.setText("");
+            yearRegistration.setText("");
+            hourRegistration.setText("");
+            minuteRegistration.setText("");
+            secondRegistration.setText("");
+        }
+        //Заполнение даты и времени желаемой даты реализации
+        OffsetDateTime wishDateTask = selectedTask.getValue().getWishDate();
+        if(wishDateTask != null) {
+            dayWish.setText(Integer.toString(wishDateTask.getDayOfMonth()));
+            monthWish.setText(Integer.toString(wishDateTask.getMonthValue()));
+            yearWish.setText(Integer.toString(wishDateTask.getYear()));
+            hourWish.setText(Integer.toString(wishDateTask.getHour()));
+            minuteWish.setText(Integer.toString(wishDateTask.getMinute()));
+            secondWish.setText(Integer.toString(wishDateTask.getSecond()));
+        } else {
+            dayWish.setText("");
+            monthWish.setText("");
+            yearWish.setText("");
+            hourWish.setText("");
+            minuteWish.setText("");
+            secondWish.setText("");
+        }
+        //Заполнение даты и времени даты выполнения задачи
+        OffsetDateTime completeDateTask = selectedTask.getValue().getCompleteDate();
+        if(completeDateTask != null) {
+            dayComplete.setText(Integer.toString(completeDateTask.getDayOfMonth()));
+            monthComplete.setText(Integer.toString(completeDateTask.getMonthValue()));
+            yearComplete.setText(Integer.toString(completeDateTask.getYear()));
+            hourComplete.setText(Integer.toString(completeDateTask.getHour()));
+            minuteComplete.setText(Integer.toString(completeDateTask.getMinute()));
+            secondComplete.setText(Integer.toString(completeDateTask.getSecond()));
+        } else {
+            dayComplete.setText("");
+            monthComplete.setText("");
+            yearComplete.setText("");
+            hourComplete.setText("");
+            minuteComplete.setText("");
+            secondComplete.setText("");
+        }
+        //Проставление статуса выполнения задачи
+        StatusEnum completeCheckStatus = selectedTask.getValue().getStatus();
+        if(completeCheckStatus == StatusEnum.COMPLETE) {
+            checkComplete.setSelected(true);
+        } else {
+            checkComplete.setSelected(false);
+        }
+        //Заполнение описания задачи
+        String descriptionTask = selectedTask.getValue().getDescription();
+        if(descriptionTask != null) {
+            description.setText(descriptionTask);
+        } else {
+            description.setText("");
+        }
     }
 
     /**
@@ -170,7 +212,7 @@ public class Controller implements Initializable {
         if(Task.mainTreeNode == null) Task.mainTreeNode = new TreeItem<Task>(new Task("Задачи"));
         try (
                 Statement statement = connection.createStatement();
-                ResultSet resultSet = statement.executeQuery("SELECT * FROM tasks")
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM tasks");
         ) {
             while(resultSet.next()) {
                 tasks.add(mapRowTask(resultSet));
